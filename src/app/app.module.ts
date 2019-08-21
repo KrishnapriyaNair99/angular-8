2dfd5a89a9977c4c6f4c81241b38004413b40859
krishnapriya <krishnapriya.nair@v2solutions.com>
2019-08-21 11:48:34 +0530

    The basics section of angular-8 3 Assignments Components Data binding Directives

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertsComponent } from './success_alerts/success_alerts.component';
+import { WarningAlertsComponent } from './warning_alerts/warning_alerts.component';
+import { DataBindingComponent } from './assignment-2-data-binding/data-binding.component';
+import { DirectivesComponent } from './assignment-3-directives/directives.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertsComponent,
+    SuccessAlertsComponent,
+    DataBindingComponent,
+    DirectivesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
